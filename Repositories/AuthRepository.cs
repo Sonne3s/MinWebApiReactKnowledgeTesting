f988935527c24b44017fcc7160acf8e19dd31450
Sonne3s <sonne.mobstyle@gmail.com>
2023-08-30 13:04:09 +0300

    Create database from EF CodeFirst

---
+++ Repositories/AuthRepository.cs
@@ -0,0 +1,17 @@
+﻿using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class AuthRepository
+    {
+        public Auth? Get(string login, string password)
+        {
+            using (var db = new Context()) { return db.Auths.SingleOrDefault(a => a.Login == login && a.Password == password); }
+        }
+    }
+}
---
+++ Repositories/UsersRepository.cs
@@ -0,0 +1,21 @@
+﻿using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class UsersRepository
+    {
+        private Context? _db;
+
+        public User? Get(Guid id)
+        {
+            using (var db = new Context()) { return db.Users.FirstOrDefault(u => u.Id == id); }
+        }
+
+        //public User? Get1(Guid id) => Context.Execute(() => _db.Users.FirstOrDefault(u => u.Id == id), out _db);
+    }
+}
